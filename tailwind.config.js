24884ddba0c14ba0d45680c3f8c7589e478c7a1f
amirmahdirahimzadeh2009 <158206164+amirmahdirahimzadeh2009@users.noreply.github.com>
2024-05-03 02:01:35 +0330

    the-end responsive

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      background: "#1A1A1A",
+      button: "#000000",
+      gray: "#A8A8A8",
+      white: "#ffffff",
+      darkwhite: "#F4F7FA",
+      orange: "#FF9142",
+      grayi: "#606060",
+      gray2: "#949494",
+      footer: "#131313",
+    },
+    fontFamily: {
+      Rubik: ["Rubik"],
+      syne: ["syne"],
+      Inter: ["Inter"],
+      opensans: ["Open+Sans"],
+    },
+  },
+  plugins: [],
+};
